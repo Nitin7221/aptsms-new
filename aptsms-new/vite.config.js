9e85c9a6734867881343c0a69908aa323e716839
Nitin Bhandari <nitin211206@gmail.com>
2025-06-10 22:46:12 +0530

    Deployment on vercel

--- aptsms-new/vite.config.js
+++ aptsms-new/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE_PATH || "/aptsms-new",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
